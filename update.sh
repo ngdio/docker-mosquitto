08c8488247cf8a0672ad28bdfd245fe5b358b624
Niklas <dev@n1klas.net>
2018-07-22 19:17:06 +0200

    Change update script to change versions instead of regenerating from a template

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "No version specified"
+fi
+
 DIR_ARCHS=(amd64 i386 armhf aarch64)
-IMAGE_ARCHS=(amd64 i386 arm32v6 arm64v8)
 
 for index in ${!DIR_ARCHS[*]}; do
-    sed -e "s/%%ARCH%%/${IMAGE_ARCHS[$index]}/g" Dockerfile > ${DIR_ARCHS[$index]}/Dockerfile
+    sed -i -e "s/^ENV VERSION .*$/ENV VERSION $1/g" ${DIR_ARCHS[$index]}/Dockerfile
 done
