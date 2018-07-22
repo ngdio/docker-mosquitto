36439c6021b1ad06b312420b239dd5ae9e592753
Niklas <dev@n1klas.net>
2018-07-22 20:42:30 +0200

    Fix link to QEMU binaries

--- prepare-qemu.sh
+++ prepare-qemu.sh
@@ -7,6 +7,6 @@
 QEMU_VERSION=2.12.0
 
 for arch in i386 arm aarch64; do
-    wget https://github.com/multiarch/qemu-user-static/releases/download/v$QEMU_VERSION/x86_64_qemu-aarch64-static.tar.gz \
+    wget https://github.com/multiarch/qemu-user-static/releases/download/v$QEMU_VERSION/x86_64_qemu-$arch-static.tar.gz \
         -O qemu-$arch.tar.gz
 done
